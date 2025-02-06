c616e2ca66a8699e93a4f2126f503781876375a6
Andreas Kugel <andreas.kugel@web.de>
2025-02-06 11:12:04 +0100

    added vitest.config

---
+++ vitest.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,         // Enable global test methods like describe, it
+    //setupFiles: './test/setup.ts', // Include setup file
+    coverage: {
+      reporter: ['text', 'html'],
+    },
+    browser: {
+      provider: "webdriverio", //'playwright', // or 'webdriverio'
+      enabled: true,
+      name: 'firefox', // browser name is required
+    },
+  },
+});
+
